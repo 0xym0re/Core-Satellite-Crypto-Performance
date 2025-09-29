d7763a445399016836c7ea08da8a0d300a871f56
0xym0re <martincourtois.eth@gmail.com>
2025-09-29 14:07:13 +0200

    Update shared_assets.py

--- pages/shared_assets.py
+++ pages/shared_assets.py
@@ -5,7 +5,7 @@ asset_mapping = {
     "US 10Y Yield": "^TNX",
     "Dollar Index": "UUP",
     "Gold": "IAU",
-    "iShares Core Global Aggregate Bond (Acc)": "AGAC.AS",
+    "iShares Core Global Aggregate Bond (Acc)": "AGG",
     "iShares Global REITs": "VNQ",
 }
 crypto_static = {
